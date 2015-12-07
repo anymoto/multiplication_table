cfc7a77e3db37eb715eb66d081fdc3913b3d573f
anymoto <ana@castromartinez.com>
2015-12-06 21:29:00 -0600

    Use arguments instead of asking for input

--- print_table.rb
+++ print_table.rb
@@ -1,11 +1,10 @@
 #!/usr/bin/env ruby
 require './lib/multiplication_table'
 
-puts 'How many prime numbers do you want?'
-size = gets.to_i
+size = ARGV.first.to_i
 
 if size < 1
-  puts 'This quantity is not valid. Going to use the default that is 10 instead.'
+  puts 'Quantity is not present or valid. Going to use the default that is 10 instead.'
   size = 10
 end
 
